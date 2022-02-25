e48e0e1d7c2eabdf1cf11c50795d7ab5ed0346e0
Clifton Ho <cliftonho311@gmail.com>
2022-02-25 08:27:37 -0800

    two_sum with map

--- exercises/LC1_TwoSum/index.js
+++ exercises/LC1_TwoSum/index.js
@@ -6,6 +6,20 @@
 // twoSum([2,7,11,15], 9) --> [0,1]
 // twoSum([3,5,4], 9) --> [1,2]
 
-function twoSum(arr, target) {}
+function twoSum(arr, target) {
+  let map = new Map()
+    
+  for (let i = 0; i < arr.length; i++) {
+    let complement = target - arr[i];
+      
+      if (map.has(complement)) {
+          return [ i, map.get(complement)]
+      } else {
+          map.set(arr[i], i)
+      }
+  }
+
+
+}
 
 module.exports = twoSum;
