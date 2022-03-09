c6b90c71a90926c55bb4b7f52cd6bf0b5282decb
Clifton Ho <cliftonho311@gmail.com>
2022-03-08 21:22:07 -0800

    invertTree

--- exercises/LC226_invertTree/index.js
+++ exercises/LC226_invertTree/index.js
@@ -1,3 +1,16 @@
-function invertTree(root) {}
+function invertTree(root) {
+
+  if (!root) return
+
+  let temp = root.left;
+  root.left = root.right
+  root.left = temp
+
+  invertTree(root.left)
+  invertTree(root.right)
+
+
+  return root
+}
 
 module.exports = invertTree;
